2be8fcfe3245430c71c2f36a3a04946986a3bbc0
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-04-30 06:56:18 +0300

    add  html for comments and pitch

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 # Initializing application
@@ -14,6 +18,8 @@ def create_app(config_name):
 
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
+
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255),unique = True,nullable = False)
@@ -36,6 +39,7 @@ class Pitch(db.Model):
     upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    time = db.Column(db.DateTime, default = datetime.utcnow)
     category = db.Column(db.String(255), index = True,nullable = False)
     
     def save_p(self):
@@ -45,3 +49,29 @@ class Pitch(db.Model):
         
     def __repr__(self):
         return f'Pitch {self.post}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text(),nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+
+    def save_c(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,pitch_id):
+        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+
+        return comments
+
+    
+    def __repr__(self):
+        return f'comment:{self.comment}'
+        
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
