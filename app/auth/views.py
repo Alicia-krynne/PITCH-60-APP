f395bf353845521275793bc57ca68ac57016ce9f
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-04-30 13:50:20 +0300

    welcome user info

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,6 +6,7 @@ from ..models import User
 from .. import db
 from ..email import mail_message
 
+
 @auth.route('/login', methods = ['GET','POST'])
 def login():
     form = LoginForm()
@@ -29,6 +30,6 @@ def signup():
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         user.save_u()
-        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch-60-App","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     return render_template('auth/signup.html', r_form = form)
--- config.py
+++ config.py
@@ -5,9 +5,10 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-
-
-
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macrine:Alicemacrine!@localhost/pitch'
    
