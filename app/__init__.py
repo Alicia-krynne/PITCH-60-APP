13f5392554d042bd68529da66ffac0c2e3ded5ae
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-03 14:55:09 +0300

    images.jpg

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,10 @@ login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
 
-
-
 # Initializing application
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
-
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
