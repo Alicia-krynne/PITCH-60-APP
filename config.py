3bfea55b71a2199300e6c9751f75cbfcea2534dd
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-04 12:34:03 +0300

    new database url

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI='postgresql://foozzhvvhqpsgm:6e03d4d2f7a47eff5af26c93078a4136efbe5cf74c657bc33e6024653316589a@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d3q6asgrmtqln'
+    SQLALCHEMY_DATABASE_URI='postgresql://foozzhvvhqpsgm:6e03d4d2f7a47eff5af26c93078a4136efbe5cf74c657bc33e6024653316589a@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d3q6asgrmtqln1'
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macrine:Alicemacrine!@localhost/pitch'
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
@@ -12,7 +12,8 @@ class Config:
     MAIL_USE_TLS = True
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI='postgresql://foozzhvvhqpsgm:6e03d4d2f7a47eff5af26c93078a4136efbe5cf74c657bc33e6024653316589a@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d3q6asgrmtqln'
+    SQLALCHEMY_DATABASE_URI='postgresql://foozzhvvhqpsgm:6e03d4d2f7a47eff5af26c93078a4136efbe5cf74c657bc33e6024653316589a@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d3q6asgrmtqln1'
+
     #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macrine:Alicemacrine!@localhost/pitch'
    
 class DevConfig(Config):
