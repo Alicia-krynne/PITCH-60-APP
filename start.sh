2e303c36f020e04e74a96836d4f019ea84ca2596
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-04 13:13:17 +0300

    update data url

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='iloveramen!'
 export MAIL_USERNAME='attackonbangtan553@gmail.com'
 export MAIL_PASSWORD='bangtan2020!'
-export DATABASE_URL='postgresql://foozzhvvhqpsgm:6e03d4d2f7a47eff5af26c93078a4136efbe5cf74c657bc33e6024653316589a@ec2-23-22-191-232.compute-1.amazonaws.com:5432/d3q6asgrmtqln1'
+
 
 
 python3 manage.py server
